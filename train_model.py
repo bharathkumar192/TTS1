0f16badecbba12f71397edfb2c76a6a4a3860112
Bharath Kumar <bharathkumar1922001@gmail.com>
2025-04-27 02:56:40 -0400

    minor

--- train_model.py
+++ train_model.py
@@ -13,7 +13,9 @@ from trainer import Trainer, TrainerArgs
 
 from TTS.config.shared_configs import BaseDatasetConfig
 from TTS.tts.datasets import load_tts_samples
-from TTS.tts.layers.xtts.trainer.gpt_trainer import GPTArgs, GPTTrainer, GPTTrainerConfig, XttsAudioConfig
+from TTS.tts.layers.xtts.trainer.gpt_trainer import GPTArgs, GPTTrainer, GPTTrainerConfig
+from TTS.tts.models.xtts import  XttsAudioConfig
+
 # from TTS.utils.manage import ModelManager # Not needed if paths are passed directly
 
 
